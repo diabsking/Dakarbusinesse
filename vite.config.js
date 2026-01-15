be491451838d19a2aae6e890d8a1cda79ca19192
Diabal DIEYE <dieyediabal75@gmail.com>
2026-01-15 22:29:32 +0000

    prépare frontend pour déploiement Render

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: resolve(__dirname, "."), // le dossier où se trouve index.html et package.json
+  build: {
+    outDir: "dist", // dossier de sortie pour le build
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
     open: true,
