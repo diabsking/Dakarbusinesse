9ff30bfe43ede1c5fcbce6799fff9d05e8ba1c0c
Diabal DIEYE <dieyediabal75@gmail.com>
2026-01-16 01:32:44 +0000

    fix: update HomeLayout layout

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  // root: resolve(__dirname, "src"), // retiré pour éviter src/src
-  build: {
-    outDir: resolve(__dirname, "dist"), // sortie finale à la racine
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"), // @ pointe vers src/
-    },
-  },
   server: {
     port: 5173,
     open: true,
